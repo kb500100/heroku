c9926470a65b41deaf578cd38937a1e311ced559
Keldrick Mitchell <Keldrick.brown11@gmail.com>
2019-02-22 14:54:45 -0600

    Changed main file to index.js

--- public/server.js
+++ public/server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.render('index.js');
+	res.render('index');
 })
 
 app.listen(port, function() {
